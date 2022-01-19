34a0843c71feac406934a60c627620ff4cdc82fd
Julia Kozak <jkozak30@stuy.edu>
2022-01-19 18:24:38 -0500

    functions

--- v3/Minesweeper.java
+++ v3/Minesweeper.java
@@ -7,6 +7,7 @@ public class Minesweeper {
   int playState;
   final String letters = "ABCDEFGHIJKLMNOPQRSTUVXYZ!@#$%^&*() ";
 
+
   public Minesweeper(int r, int c, int m) {
     playState = 0;
     rows = r;
@@ -15,8 +16,8 @@ public class Minesweeper {
     squareVals = new int[rows+2][columns+2];
     for (int i=0; i<rows+2; i++) {
       for (int j=0; j<columns+2; j++) {
-        squares[i][j] = "sad extreme";
         squareVals[i][j] = 9;
+        squares[i][j] = "@_@";
       }
     }
     for (int i=1; i<rows+1; i++) {
@@ -26,10 +27,10 @@ public class Minesweeper {
       }
     }
     while(mines != m) {
-      int rI = (int)(Math.random()*(rows))+1;
-      int cI = (int)(Math.random()*(columns))+1;
-      if (squareVals[rI][cI] != -1) {
-        squareVals[rI][cI] = -1;
+      int rIndex = (int)(Math.random()*(rows)) + 1;
+      int cIndex = (int)(Math.random()*(columns)) + 1;
+      if (squareVals[rIndex][cIndex] != -1) {
+        squareVals[rIndex][cIndex] = -1;
         mines ++;
       }
     }
@@ -48,25 +49,14 @@ public class Minesweeper {
         }
       }
     }
+
+
+
   }
 
-  public void checkWin(){
-    int ctr = 0;
-    for (int i=1; i<rows+1; i++) {
-      for (int j=1; j<columns+1; j++) {
-        if (!(isRevealed(i, j))) {
-          ctr++;
-        }
-      }
-    }
-    if (ctr == mines) {
-      playState = 1;
-    }
-  }
-
-  public void processInput(String e) {
-    int cCor = letters.indexOf(e.substring(0, 1))+1;
-    int rCor = Integer.parseInt(e.substring(1));
+  public void processInput(String e){
+    int cCor = letters.indexOf(e.substring(0, 1)) + 1;
+    int rCor = (Integer.parseInt(e.substring(1)));
     if (squareVals[rCor][cCor] != -1) {
       reveal(rCor, cCor);
     } else {
@@ -74,64 +64,87 @@ public class Minesweeper {
     }
   }
 
-  public void reveal(int cCor, int rCor) {
-    if (squareVals[cCor][rCor] != -1) {
-      if (!(isRevealed(cCor, rCor))) {
-          squares[cCor][rCor] = Integer.toString(squareVals[cCor][rCor]);
-      }
-      if (squareVals[cCor][rCor] == 0) {
-        for (int p = cCor-1; p<cCor+2; p++) {
-          for (int q=rCor-1; q<rCor+2; q++) {
-    	      if (p != rCor || q != cCor) {
-    		if (!(isRevealed(p,q))) reveal(p,q);
-    	      }
-    	  }
+  public void reveal(int rCor, int cCor) {
+    if (squareVals[rCor][cCor] != -1) {
+      if (squareVals[rCor][cCor] != 0) {
+        squares[rCor][cCor] = Integer.toString(squareVals[rCor][cCor]);
+      } else {
+        squares[rCor][cCor] = " ";
+        for (int i=rCor-1; i<rCor+2; i++) {
+          for (int j=cCor-1; j<cCor+2; j++) {
+            if (i != rCor || j != cCor) {
+              if (!(isRevealed(i,j))) reveal(i, j);
+            }
+          }
         }
       }
     }
   }
 
-  public boolean isRevealed(int cCor, int rCor) {
-    return !(squares[cCor][rCor].equals("█") || squares[cCor][rCor].equals("⚑"));
+  public boolean isRevealed(int rCor, int cCor) {
+    return !(squares[rCor][cCor].equals("█"));
   }
 
   public void printBoard() {
-    System.out.print("    ");
+    System.out.print("   ");
     for (int i=0; i<columns; i++) {
-      System.out.print(letters.substring(i, i+1) + " ");
+      System.out.print(" " + letters.substring(i, i+1));
     }
     System.out.println();
     for (int i=1; i<rows+1; i++) {
       System.out.print(i);
-      System.out.print(" ");
-      if (i <= 9) {
+      if (Integer.toString(i).length() > 1) {
         System.out.print(" ");
+      } else {
+        System.out.print("  ");
       }
       for (int j=1; j<columns+1; j++) {
-        System.out.print("|" + squares[i][j]);
+        System.out.print("|" + squares[i][j] );
       }
       System.out.println("|");
     }
   }
 
+  public void lose() {
+    System.out.println("bad");
+  }
+
+  public void checkWin() {
+    int mCtr = 0;
+    for (int i=1; i<rows+1; i++) {
+      for (int j=1; j<columns+1; j++) {
+        if (squares[i][j].equals("█")) {
+          mCtr++;
+        }
+      }
+    }
+    if (mCtr == mines) {
+      playState = 1;
+    }
+  }
+
+  public void win() {
+    printBoard();
+    System.out.println("YOU WON GOOD JOB");
+  }
+
   public static void main(String[] args) {
-    Minesweeper you;
-    int r, c, m;
     Scanner response = new Scanner(System.in);
+    int r, c, m;
 
     System.out.println("welcome to minesweeper");
     System.out.println();
 
-    System.out.print("choose rows: ");
+
+    System.out.print("choose length: ");
     String input = response.nextLine();
     try {
       r = Integer.parseInt(input);
     } catch (Exception e) {
       r = 10;
     }
-    System.out.println();
 
-    System.out.print("choose columns: ");
+    System.out.print("choose width: ");
     input = response.nextLine();
     try {
       c = Integer.parseInt(input);
@@ -139,15 +152,19 @@ public class Minesweeper {
       c = 10;
     }
 
-    System.out.print("choose mines: ");
+    System.out.print("number of mines: ");
     input = response.nextLine();
     try {
       m = Integer.parseInt(input);
     } catch (Exception e) {
       m = 20;
     }
+    System.out.println();
 
-    if (r > 0 && c > 0 && r <= 99 && c <= 35 && m >= 0 && m <= r*c) {
+    //game start
+    Minesweeper you;
+
+    if (r >= 1 && c >= 1 && m >= 0 && m <= r*c) {
       you = new Minesweeper(r, c, m);
     } else {
       you = new Minesweeper(10, 10, 20);
@@ -156,7 +173,7 @@ public class Minesweeper {
 
     while(you.playState == 0) {
       you.printBoard();
-      System.out.print("your move: ");
+      System.out.print("your move:");
       input = response.nextLine();
       try {
         you.processInput(input);
@@ -166,7 +183,13 @@ public class Minesweeper {
       you.checkWin();
     }
 
+    if (you.playState == 1) {
+      you.win(); //!!
+    } else {
+      you.lose(); //smh
+    }
 
+    //you.printBoard();
 
 
   }
